f3a526437d7be8de926f166cc111ce11f9988aac
Camila <hello@camila.codes>
2022-02-06 14:50:35 +0100

    Fix unmerged parts of the code when rebasing PR #4186.
    Signed-off-by: Camila <hello@camila.codes>

--- src/gui/tray/activitylistmodel.cpp
+++ src/gui/tray/activitylistmodel.cpp
@@ -282,17 +282,13 @@ QVariant ActivityListModel::data(const QModelIndex &index, int role) const
     case DisplayActions:
         return _displayActions;
     case ShareableRole:
-<<<<<<< HEAD
         return !data(index, PathRole).toString().isEmpty() && a._objectType == QStringLiteral("files") && _displayActions && a._fileAction != "file_deleted" && a._status != SyncFileItem::FileIgnored;
     case IsCurrentUserFileActivityRole:
         return a._isCurrentUserFileActivity;
-=======
-        return !data(index, PathRole).toString().isEmpty() && _displayActions && a._fileAction != "file_deleted" && a._status != SyncFileItem::FileIgnored;
     case TalkConversationTokenRole:
         return a._talkNotification.conversationToken;
     case TalkMessageIdRole:
         return a._talkNotification.messageId;
->>>>>>> 5a51de371 (Add talk token and messageId to data roles in ActivityListModel.)
     default:
         return QVariant();
     }
